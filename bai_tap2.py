b8f1f8f8775e3a9756dc52ac7a816d342547051f
Thiện Nguyễn <thiennguyen03001@gmail.com>
2025-03-22 10:59:29 +0700

    Add files via upload

---
+++ bai_tap2.py
@@ -0,0 +1,22 @@
+num_one = 5
+num_two = 4
+
+total = num_one+num_two
+diff = num_two-num_one
+product = num_one*num_two
+division = num_one/num_two
+remainder = num_two%num_one
+exp = num_one**num_two
+
+import math
+
+floor = math.floor(num_one/num_two)
+
+
+print("Tổng:",total)
+print("Hiệu:",diff)
+print("Tích:",product)
+print("Thương:",division)
+print("Dư :",remainder)
+print("Mũ:",exp)
+print("Floor:",floor)
